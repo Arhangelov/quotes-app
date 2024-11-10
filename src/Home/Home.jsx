f60f8ea9743ca206cf26cec6aff634d442825b67
Arhangelov <arhangelov93@gmail.com>
2024-11-10 17:11:31 +0200

    initial commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import QuoteList from '../QuoteList/QuoteList'
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <QuoteList />
+        </div>
+    )
+}
+
+export default Home
---
+++ src/QuoteList/QuoteList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./QuoteList.css";
+
+const QuoteList = () => {
+    return (
+        <div>QuoteList</div>
+    )
+}
+
+export default QuoteList
