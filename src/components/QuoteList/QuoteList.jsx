37580d14101b139e07047cd26cf89591c7e3daf2
Arhangelov <arhangelov93@gmail.com>
2024-11-10 20:28:38 +0200

    created quote list component

--- src/components/QuoteList/QuoteList.jsx
+++ src/components/QuoteList/QuoteList.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 import "./QuoteList.css";
 
+//Context
+import { useQuotes } from '../context/quoteContext';
+
+//Services
+import { getQuoteService } from '../../services/homeServices';
+
+//Icons
+import { IoIosRefresh } from "react-icons/io";
+
 const QuoteList = () => {
+    const [quotes, setQuotes] = useQuotes();
+    const refreshHandle = (category) => {
+        getQuoteService(category)
+            .then(newQuote => {
+                setQuotes(newQuote)
+            })
+    }
+
     return (
-        <div>QuoteList</div>
+        <div className='quote-list-container'>
+            {quotes?.map(quote => (
+                <div className="quote-card">
+                    <blockquote>
+                        <p className='text'>{quote.quote}</p>
+                    </blockquote>
+                    <cite>—{quote.author}</cite>
+                    <button onClick={() => refreshHandle(quote.category)} className='btn'><IoIosRefresh /></button>
+                </div>
+            ))}
+        </div>
     )
 }
 
